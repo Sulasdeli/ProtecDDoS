464e4492ae17ede7a7067e060768177b5069862f
Sulasdeli <erion.sula@uzh.ch>
2019-05-19 17:39:35 +0200

    adjustment to Scatter Plot

--- client/src/components/ExplorePage.jsx
+++ client/src/components/ExplorePage.jsx
@@ -143,9 +143,7 @@ class ExplorePage extends Component {
         })
             .then(res => res.json())
             .then(jsonResponse => {
-
-                console.log(jsonResponse)
-
+                
                 this.setState({
                     ...this.state,
                     services: jsonResponse.recommendedServices,
--- client/src/views/scatterPlot.js
+++ client/src/views/scatterPlot.js
@@ -1,19 +1,12 @@
 import React from 'react'
-import {Highcharts3dChart, Chart, withHighcharts, XAxis, YAxis, ZAxis, Title, Subtitle, ScatterSeries}
-        from 'react-jsx-highcharts'
+import {Highcharts3dChart, withHighcharts, XAxis, YAxis, ZAxis, Tooltip, ScatterSeries, Scatter3dSeries}
+    from 'react-jsx-highcharts'
 import Highcharts from 'highcharts';
 import addHighcharts3DModule from 'highcharts/highcharts-3d';
 import styled from "styled-components";
 import {Card} from "@material-ui/core";
 import CardHeader from "./CardHeader";
 addHighcharts3DModule(Highcharts);
-
-
-const getData = (services, userIndex) =>{
-    let data = [userIndex];
-    services.forEach( s => data.push(JSON.parse(s.weightedSimilarity)));
-    return data
-};
 
 const ChartContainer = styled.div`
   margin-bottom: 45px;
@@ -22,20 +15,25 @@ const ChartContainer = styled.div`
 const ScatterPlot = ({services, userIndex}) => (
     <ChartContainer>
         <Card style={{borderRadius: "10px 10px 10px 10px"}}>
-            <CardHeader title='Scatter Plot' iconName='vcard-o' backgroundColor='linear-gradient(60deg, #66bb6a, #43a047)'/>
-            <Highcharts3dChart alpha="20" beta="25" depth="100">
+            <CardHeader title='Scatter Plot' backgroundColor='linear-gradient(0deg, #66bb6a, #43a047)'/>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <Highcharts3dChart alpha="20" beta="20" depth="270">
+                        <Tooltip/>
+                        <XAxis min={0}>
+                            <XAxis.Title style={{fontWeight: "bold", fontSize: 15}} margin={40}>Deployment Time</XAxis.Title>
+                        </XAxis>
+                        <YAxis min={0}>
+                            <YAxis.Title style={{fontWeight: "bold", fontSize: 15}} margin={40}>Leasing Period</YAxis.Title>
+                        </YAxis>
+                        <ZAxis min={0}>
+                            <ZAxis.Title style={{fontWeight: "bold", fontSize: 15}} skex3d={true} margin={-400}>Budget</ZAxis.Title>
+                            <Scatter3dSeries name="User Profile Index" color={"red"} data={[userIndex]}/>
 
-                <XAxis min={0}>
-                    <XAxis.Title>Deployment Time</XAxis.Title>
-                </XAxis>
-                <YAxis min={0}>
-                    <YAxis.Title>Leasing Period</YAxis.Title>
-                </YAxis>
-                <ZAxis min={0}>
-                    <ZAxis.Title>Budget</ZAxis.Title>
-                    <ScatterSeries data={getData(services,userIndex)} />
-                </ZAxis>
-            </Highcharts3dChart>
+                            {services.map((s, i) => <ScatterSeries key={i} name={s.providerName} data={[JSON.parse(s.weightedSimilarity)]}/>)}
+                        </ZAxis>
+                    </Highcharts3dChart>
+
+            </div>
         </Card>
     </ChartContainer>
 );
