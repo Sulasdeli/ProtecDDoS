21e11757df0377438ef134c6751f9bf909f28e02
Sulasdeli <erion.sula@uzh.ch>
2019-05-26 22:58:20 +0200

    add formatter

--- client/src/views/scatterPlot.js
+++ client/src/views/scatterPlot.js
@@ -19,6 +19,11 @@ const SettingsContainer = styled.div`
   align-items: center;
 `;
 
+const pointFormatter = function()  {
+    return `<strong>${this.series.name}</strong><br/>Deployment Time: ${this.point.x}<br />Leasing Period:
+            ${this.point.y}<br />Budget: ${this.point.z}`
+};
+
 class ScatterPlot extends Component {
     constructor() {
         super();
@@ -41,7 +46,7 @@ class ScatterPlot extends Component {
                     <CardHeader title='Scatter Plot' backgroundColor='linear-gradient(0deg, #66bb6a, #43a047)'/>
                     <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                         <Highcharts3dChart alpha={this.state.alpha} beta={this.state.beta} depth="300" legend={{enabled: true}}>
-                            <Tooltip/>
+                            <Tooltip formatter={pointFormatter}/>
                             <XAxis min={0}>
                                 <XAxis.Title style={{fontWeight: "bold", fontSize: 15}} margin={40}>Deployment Time</XAxis.Title>
                             </XAxis>
@@ -49,7 +54,7 @@ class ScatterPlot extends Component {
                                 <YAxis.Title style={{fontWeight: "bold", fontSize: 15}} margin={40}>Leasing Period</YAxis.Title>
                             </YAxis>
                             <ZAxis min={0}>
-                                <ZAxis.Title style={{fontWeight: "bold", fontSize: 15}} skex3d={true} margin={-400}>Budget</ZAxis.Title>
+                                <ZAxis.Title style={{fontWeight: "bold", fontSize: 15}} margin={-400}>Budget</ZAxis.Title>
                                 <Scatter3dSeries name="User Profile Index" color={"red"} lineWidth={2} data={[[0,0,0], this.props.userIndex]}/>
                                 {this.props.services.map((s, i) => <ScatterSeries key={i} lineWidth={1} name={s.providerName} data={[[0,0,0], JSON.parse(s.weightedSimilarity)]}/>)}
                             </ZAxis>
